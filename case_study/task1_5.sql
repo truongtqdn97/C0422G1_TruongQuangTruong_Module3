3ae7ee6cd255eaf7a106d5671a48fcfd4707d02b
truongtqdn97 <truong.dn97@gmail.com>
2022-07-19 21:39:50 +0700

    task 5

--- case_study/task1_5.sql
+++ case_study/task1_5.sql
@@ -33,7 +33,7 @@ where lk.ten_loai_khach = "Diamond"
 select kh.ma_khach_hang, kh.ho_ten, lk.ten_loai_khach, 
 	hd.ma_hop_dong, dv.ten_dich_vu, 
 	hd.ngay_lam_hop_dong, hd.ngay_ket_thuc, 
-    dvdk.ten_dich_vu_di_kem, (dv.chi_phi_thue + hdct.so_luong * dvdk.gia) as tong_tien
+    dvdk.ten_dich_vu_di_kem, ( ifnull(dv.chi_phi_thue, 0) + ifnull(hdct.so_luong, 0) * ifnull(dvdk.gia, 0)) as tong_tien
 from khach_hang kh
 	left join hop_dong hd on kh.ma_khach_hang = hd.ma_khach_hang
     left join dich_vu dv on hd.ma_dich_vu = dv.ma_dich_vu
