bf3001a479e6355c463f66f7b34e77433cd78056
truongtqdn97 <truong.dn97@gmail.com>
2022-07-19 22:00:44 +0700

    fix

---
+++ case_study/task11_15.sql
@@ -0,0 +1,33 @@
+use case_study;
+
+
+-- 11.Hiển thị thông tin các dịch vụ đi kèm đã được sử dụng bởi những khách hàng có ten_loai_khach là “Diamond” 
+-- và có dia_chi ở “Vinh” hoặc “Quảng Ngãi”.
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- 12.Hiển thị thông tin ma_hop_dong, ho_ten (nhân viên), ho_ten (khách hàng), so_dien_thoai (khách hàng), 
+-- ten_dich_vu, so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem), tien_dat_coc
+--  của tất cả các dịch vụ đã từng được khách hàng đặt vào 3 tháng cuối năm 2020 
+-- nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2021.
+
+-- 13.Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng. 
+-- (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
+
+
+-- 14.	Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất. 
+-- Thông tin hiển thị bao gồm ma_hop_dong, ten_loai_dich_vu, ten_dich_vu_di_kem, 
+-- so_lan_su_dung (được tính dựa trên việc count các ma_dich_vu_di_kem).
+
+-- 15.	Hiển thi thông tin của tất cả nhân viên bao gồm ma_nhan_vien, ho_ten, ten_trinh_do, ten_bo_phan, 
+-- so_dien_thoai, dia_chi mới chỉ lập được tối đa 3 hợp đồng từ năm 2020 đến 2021.
--- ss3/exercise/exercise1.sql
+++ ss3/exercise/exercise1.sql
@@ -1,42 +1,42 @@
-USE quanlysinhvien;
+use quanlysinhvien;
 
 -- Hiển thị tất cả các sinh viên có tên bắt đầu bảng ký tự ‘h’
-SELECT 
-    studentid, stuDENTNAme
-FROM
-    stuDent
-WHERE
-    STUdentName LIKE 'H%';
+select 
+    studentid, studentname
+from
+    student
+where
+	studentname LIKE 'H%';
 
 -- Hiển thị cáC THông tin lớp học có thời gian bắt đầu vào tháng 12.
-SELECT 
+select 
     *
-FROM
+from
     class
-WHERE
-    MONTH(sTARTDAte) = '12';
+where
+    month(startdate) = '12';
 
 
 -- Hiển tHỊ TẤt cả CÁC THông tin mÔn học có credit trong khoảng từ 3-5.
-SELECT *
-FROM subject
-WHERE Credit BETWEEN 3 AND 5;
--- WHERE CreDIT>=3 AnD CRedit<=5;
+select *
+from `subject`
+where Credit between 3 and 5;
+-- where CreDIT>=3 and CRedit<=5;
 
 
 -- Thay đổi MÃ LỚP(ClASSID) của sinh viên có tên ‘Hung’ là 2.
-SET sql_safe_updates = 0;
-UPDATE student 
-SET 
+set sql_safe_updates = 0;
+update student 
+set 
     classid = 2
-WHERE
-    studentNAMe = 'hung';
-SET sqL_SafE_UPDAtes = 1;
+where
+    studentname = 'hung';
+set sqL_Safe_updates = 1;
 
 -- Hiển thị các ThÔnG TIN: StudentName, SuBName, MarK. Dữ liệu sắp xếp thEo điểm thi (mark) giảm dần. nếu trùng sắp theo tên tăng dần.
-SELECT s.studentname, sub.subname, m.mark
-FROM student s
-	JOIN mark m ON s.StudentId = m.StudentId
-    JOIN subject sub ON m.SubId = sub.SubId
-ORDER BY m.Mark DESC, s.StudentName;
+select s.studentname, sub.subname, m.mark
+from student s
+	join mark m on s.StudentId = m.StudentId
+    join `subject` sub on m.SubId = sub.SubId
+order by m.Mark desc, s.StudentName;
 
