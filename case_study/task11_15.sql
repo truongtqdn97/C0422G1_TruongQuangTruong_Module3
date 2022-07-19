df7f76c6543c97d35b3ff2d0f3a0991eab4856e0
truongtqdn97 <truong.dn97@gmail.com>
2022-07-20 00:12:36 +0700

    task 11 12 13

--- case_study/task11_15.sql
+++ case_study/task11_15.sql
@@ -3,26 +3,58 @@ use case_study;
 
 -- 11.Hiển thị thông tin các dịch vụ đi kèm đã được sử dụng bởi những khách hàng có ten_loai_khach là “Diamond” 
 -- và có dia_chi ở “Vinh” hoặc “Quảng Ngãi”.
-
-
-
-
-
-
-
-
-
-
-
+select dvdk.ma_dich_vu_di_kem, dvdk.ten_dich_vu_di_kem
+from dich_vu_di_kem dvdk
+	join hop_dong_chi_tiet hdct on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+    join hop_dong hd on hdct.ma_hop_dong = hd.ma_hop_dong
+    join khach_hang kh on hd.ma_khach_hang = kh.ma_khach_hang
+    join loai_khach lk on kh.ma_loai_khach = lk.ma_loai_khach
+where lk.ten_loai_khach = 'Diamond'
+	and (kh.dia_chi like "%Vinh" or kh.dia_chi like "%Quảng Ngãi");
 
 
 -- 12.Hiển thị thông tin ma_hop_dong, ho_ten (nhân viên), ho_ten (khách hàng), so_dien_thoai (khách hàng), 
 -- ten_dich_vu, so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem), tien_dat_coc
 --  của tất cả các dịch vụ đã từng được khách hàng đặt vào 3 tháng cuối năm 2020 
 -- nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2021.
+select hd.ma_hop_dong,
+	nv.ho_ten as ten_nhan_vien,
+	kh.ho_ten as ten_khach_hang,
+    kh.so_dien_thoai as sdt_khach_hang,
+    dv.ten_dich_vu,
+    sum(hdct.so_luong) as so_luong_dich_vu_di_kem,
+    hd.tien_dat_coc
+from hop_dong hd 
+	left join nhan_vien nv on hd.ma_nhan_vien = nv.ma_nhan_vien
+    left join khach_hang kh on hd.ma_khach_hang = kh.ma_khach_hang
+    left join dich_vu dv on hd.ma_dich_vu = dv.ma_dich_vu
+    left join hop_dong_chi_tiet hdct on hd.ma_hop_dong = hdct.ma_hop_dong
+where hd.ngay_lam_hop_dong between '2020/10/01' and '2020/12/31' 
+	and dv.ma_dich_vu not in (
+		select hd.ma_dich_vu
+		from hop_dong hd
+		where hd.ngay_lam_hop_dong between '2021/01/01' and '2021/06/30')
+group by kh.ma_khach_hang; -- ??? vì sao lại cần group by chỗ này
+
 
 -- 13.Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng. 
 -- (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
+select dvdk.*, sum(hdct.so_luong) as so_luong_dich_vu_di_kem
+from dich_vu_di_kem dvdk
+	join hop_dong_chi_tiet hdct on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+    group by dvdk.ma_dich_vu_di_kem
+    having so_luong_dich_vu_di_kem = (
+		select sum(hdct.so_luong) as so_luong_dich_vu_di_kem
+		from hop_dong_chi_tiet hdct
+		group by ma_dich_vu_di_kem
+		order by so_luong_dich_vu_di_kem desc
+		limit 1
+    )
+
+    
+
+
+
 
 
 -- 14.	Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất. 
