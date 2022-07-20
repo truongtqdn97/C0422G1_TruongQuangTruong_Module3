e11da5f2c5b67e80726471a1954d4ee72426341a
truongtqdn97 <truong.dn97@gmail.com>
2022-07-20 10:24:45 +0700

    task 10-15

--- case_study/task11_15.sql
+++ case_study/task11_15.sql
@@ -26,7 +26,7 @@ select hd.ma_hop_dong,
     hd.tien_dat_coc
 from hop_dong hd 
 	left join nhan_vien nv on hd.ma_nhan_vien = nv.ma_nhan_vien
-    left join khach_hang kh on hd.ma_khach_hang = kh.ma_khach_hang
+    left join khach_hang kh on hd.ma_khach_hang = kh.ma_khach_hang    
     left join dich_vu dv on hd.ma_dich_vu = dv.ma_dich_vu
     left join hop_dong_chi_tiet hdct on hd.ma_hop_dong = hdct.ma_hop_dong
 where hd.ngay_lam_hop_dong between '2020/10/01' and '2020/12/31' 
@@ -49,17 +49,44 @@ from dich_vu_di_kem dvdk
 		group by ma_dich_vu_di_kem
 		order by so_luong_dich_vu_di_kem desc
 		limit 1
-    )
-
+    );
     
-
-
-
-
 
 -- 14.	Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất. 
 -- Thông tin hiển thị bao gồm ma_hop_dong, ten_loai_dich_vu, ten_dich_vu_di_kem, 
 -- so_lan_su_dung (được tính dựa trên việc count các ma_dich_vu_di_kem).
+select hd.ma_hop_dong, 
+	dvdk.*, 
+    count(dvdk.ma_dich_vu_di_kem) as so_luong_dich_vu
+from dich_vu_di_kem dvdk
+	join hop_dong_chi_tiet hdct on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+    join hop_dong hd on hdct.ma_hop_dong = hd.ma_hop_dong
+group by dvdk.ma_dich_vu_di_kem
+	having so_luong_dich_vu = 1
+order by hd.ma_hop_dong;
+
 
 -- 15.	Hiển thi thông tin của tất cả nhân viên bao gồm ma_nhan_vien, ho_ten, ten_trinh_do, ten_bo_phan, 
 -- so_dien_thoai, dia_chi mới chỉ lập được tối đa 3 hợp đồng từ năm 2020 đến 2021.
+select nv.ma_nhan_vien,
+	nv.ho_ten as ho_ten_nhan_vien,
+    td.ten_trinh_do,
+    bp.ten_bo_phan,
+    nv.so_dien_thoai as sdt_nhan_vien,
+    nv.dia_chi
+from nhan_vien nv
+	left join hop_dong hd on nv.ma_nhan_vien = hd.ma_nhan_vien
+    left join trinh_do td on nv.ma_trinh_do = td.ma_trinh_do
+    left join bo_phan bp on nv.ma_bo_phan = bp.ma_bo_phan
+where year(hd.ngay_lam_hop_dong) in (2020, 2021)
+group by nv.ma_nhan_vien
+	having count(nv.ma_nhan_vien) <= 3;
+
+
+
+
+
+
+
+
+
